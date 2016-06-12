a1203177db9c8d8099313153831af083983a359c
Lee Jinseoung <yano24@naver.com>
2016-06-13 00:20:34 +0900

    change deploy version

--- js/index.js
+++ js/index.js
@@ -9,6 +9,8 @@ var pages = {
         if (pages[index] != undefined) { pages[index]($('section.fullpage').eq(index-1)); pages[index] = null; }
         $("#nav .item").removeClass('active').eq(index).addClass('active');
     },
+
+    // 각 페이지에 대한 에니메이션 정의 
     1: function ($page) {
         var $cube = $page.find('.shape');
         $cube.shape();
@@ -25,11 +27,18 @@ var pages = {
     2: function ($page) {
         setTimeout(() => {
             $page.find('.wrap.fullpage').addClass('brightness');
-            setTimeout(()=>{$page.find('.hidden').removeClass('hidden')},1000);
+            $page.find('#title').typeIt({
+                strings: 'To be contributer.',
+                speed: 50,
+                autoStart: false
+            });
+            setTimeout(() => {
+                $page.find('.content').fadeTo('fast',1);
+            }, 1000)
         }, 1000);
     },
     3: function ($page) {
-
+        // $('section.fullpage').eq(2).css('background', 'url("../img/goals.jpg")');
     },
     4: function ($page) {
 
@@ -44,7 +53,7 @@ var pages = {
 
 
 /*********************************************************************************
- * Navigation Module1
+ * Navigation Module
  *********************************************************************************/
 
 var navigationBar = {
@@ -79,7 +88,7 @@ $("#nav .item").on('click', function () {
     navigationBar.move($("#nav .item").index(this));
 });
 
-$('.fullpage').waitForImages(true).done(function () {
+$('.fullpage').imagesLoaded({ background: true }).done(function () {
     $('#page_loader').fadeOut();
     pages.getEvent(1);
 });
